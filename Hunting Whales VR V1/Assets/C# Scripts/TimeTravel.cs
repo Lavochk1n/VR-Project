1d9e62c1165bb5885f2fc90a8084644b2513c057
heyitsrenn <itsrennsmail@gmail.com>
2023-11-01 11:38:59 +0100

    TimeTravelEffectAdded
    Added the time travel effect and made it work with the existing time travel code

---
+++ Hunting Whales VR V1/Assets/C# Scripts/TimeTravel.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+using UnityEngine.Rendering;
+
+public class TimeTravel : MonoBehaviour
+{
+    [SerializeField] GameObject present, past;
+    [SerializeField] float effectRatePerSecond = 1;
+    [SerializeField] float cooldownTime = 1.0f;
+    [SerializeField] float transitionDuration = 0.5f;
+
+    private bool presentIsVisible = true;
+    private float timeToEffect = 0f;
+    private float lastEffectTime = 0f;
+    private XRController leftController;
+    private bool isCoolingDown = false;
+
+    public Animator animator;
+
+    void Start()
+    {
+        present.SetActive(presentIsVisible);
+        past.SetActive(!presentIsVisible);
+        leftController = GetComponent<XRController>();
+    }
+
+    void Update()
+    {
+        if (isCoolingDown)
+        {
+            if (Time.time - lastEffectTime >= cooldownTime)
+            {
+                isCoolingDown = false;
+            }
+        }
+        else
+        {
+            // Check for the "Y" button press on the left controller
+            if (leftController.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButtonValue) && secondaryButtonValue)
+            {
+                if (Time.time >= timeToEffect)
+                {
+                    timeToEffect = Time.time + 1 / effectRatePerSecond;
+                    lastEffectTime = Time.time;
+                    isCoolingDown = true;
+                    StartCoroutine(TransitionEffect());
+                }
+            }
+        }
+    }
+
+    IEnumerator TransitionEffect()
+    {
+        animator.SetTrigger("TimeTravel");
+        yield return null;
+
+        SwitchActiveLayers(); // Complete the time travel effect
+    }
+
+
+    void SwitchActiveLayers()
+    {
+        presentIsVisible = !presentIsVisible;
+        present.SetActive(presentIsVisible);
+        past.SetActive(!presentIsVisible);
+    }
+}
