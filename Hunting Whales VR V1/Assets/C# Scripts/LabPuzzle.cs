6262d336eb75e4d7fa074f67c8c57aa72e28e5b5
SamRazvan <samrazvan@gmail.com>
2023-11-06 12:50:43 +0100

    Added final puzzle scripts

---
+++ Hunting Whales VR V1/Assets/C# Scripts/LabPuzzle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabPuzzle : MonoBehaviour {
+    public GameObject snapObject; // The object that will snap to this pedestal
+    public Transform pedestal; // The pedestal where the object should snap
+    public float snapDistance = 0.1f; // Adjust this value to control the snapping distance
+
+    private bool isSnapped = false;
+
+    void Update() {
+        if (!isSnapped) {
+            float distance = Vector3.Distance(snapObject.transform.position, pedestal.position);
+
+            if (distance < snapDistance) {
+                SnapObjectToPedestal();
+            }
+        }
+    }
+
+    void SnapObjectToPedestal() {
+        snapObject.transform.position = pedestal.position;
+        snapObject.transform.rotation = pedestal.rotation;
+        snapObject.GetComponent<Rigidbody>().isKinematic = true; // Optional: make the object non-interactive
+
+        isSnapped = true;
+    }
+
+    public void ReleaseObject() {
+        snapObject.GetComponent<Rigidbody>().isKinematic = false; // Optional: make the object interactive again
+        isSnapped = false;
+    }
+}
